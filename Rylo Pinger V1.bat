dfeddcf919cb0709ffaf55da73280b81f337ee57
Hurakso <115989847+oahx@users.noreply.github.com>
2023-02-06 09:38:16 +0100

    Add files via upload

---
+++ Rylo Pinger V1.bat
@@ -0,0 +1,76 @@
+title Rylo Pinger - Made By $ D4nger AKA Rylo
+echo off & cls
+color c
+chcp 65001
+con lines=20 cols=70
+echo ██████╗ ██╗   ██╗██╗      ██████╗ 
+echo ██╔══██╗╚██╗ ██╔╝██║     ██╔═══██╗
+echo ██████╔╝ ╚████╔╝ ██║     ██║   ██║
+echo ██╔══██╗  ╚██╔╝  ██║     ██║   ██║
+echo ██║  ██║   ██║   ███████╗╚██████╔╝
+echo ╚═╝  ╚═╝   ╚═╝   ╚══════╝ ╚═════╝ 
+                                  
+echo Made With Support Of Taxen!                                                                                                                            
+set /p IP=ENTER IP OF THE VICTIM:
+echo.
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR invalid IP/URL)
+ping -t 2 0 10 127.0.0.1 >nul
+color 01
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo %IP% ERROR:)
+ping -t 2 0 10 127.0.0.1 >nul
+goto rainbow
